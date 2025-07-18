a6002a47fe8c56b651d3a603242bd0d637950b74
Sasha Le <iamsasha.le@gmail.com>
2025-07-18 10:45:30 +1000

    another fix

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import path from "path";
 import { componentTagger } from "lovable-tagger";
 
 export default defineConfig(({ mode }) => ({
+  publicDir: "public",
   base: "/",
   server: {
     host: "::",
